4a1a4b3760a2b668a0ed27c8bc372bc9c19415ec
EZYOON <44804022+EZYOON@users.noreply.github.com>
2020-05-15 15:49:16 +0900

    homework3

---
+++ homework3/practice2/routes/user.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var router = express.Router();
+let User = require('../models/user');
+let util = require('../modules/util');
+let statusCode = require('../modules/statusCode');
+let resMessage = require('../modules/responseMessage');
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+// router.post('/signup', async (req, res) => {
+//     const { id, name, password, email } = req.body;
+//     User.push({id, name, password, email});
+//     console.log(User);
+//     res.status(200).send(User);
+// });
+
+// router.post('/signup', async (req, res) => {
+//     const { id, name, password, email } = req.body;
+
+//     //예외처리 파라미터 원소가 없는 경우
+//     if ( !id || !name || !password || !email ) {
+//         //return res.status(400).send({ message: 'BAD REQUEST' });
+//         return res.status(400).send(util.fail(400,'BAD REQUEST'));
+//     }
+//     //예외처리 아이디
+//     if (User.filter(user => user.id == id).length > 0) {
+//         //return res.status(400).send({ message: 'ALREADY ID' });
+//         return res.status(400).send(util.fail(400,'ALREADY ID'));
+//     }
+
+//     User.push({id, name, password, email});
+//     //res.status(200).send(User);
+//     res.status(200).send(util.success(200, '회원가입 성공', {userId: id}));
+// });
+
+// const util = {
+//     success : (status, message, data)=>{
+//         return {
+//             status : status,
+//             success : true,
+//             message:message,
+//             data: data
+//         }
+//     },
+//     fail:(status,message)=>{
+//         return{
+//             status: status,
+//             success:false,
+//             message: message
+//         }
+//     },
+// };
+
+router.post('/signup', async (req, res) => {
+    const { id, name, password, email } = req.body;
+
+    //예외처리 파라미터 원소가 없는 경우
+    if ( !id || !name || !password || !email ) {
+        //return res.status(400).send({ message: 'BAD REQUEST' });
+        return res.status(400).send(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
+    }
+    //예외처리 아이디
+    if (User.filter(user => user.id == id).length > 0) {
+        //return res.status(400).send({ message: 'ALREADY ID' });
+        return res.status(400).send(util.fail(statusCode.BAD_REQUEST,resMessage.OUT_OF_VALUE));
+    }
+
+    User.push({id, name, password, email});
+    //res.status(200).send(User);
+    res.status(200).send(util.success(statusCode.OK, resMessage.CREATED_USER, {userId: id}));
+});
+
+router.post('/signin', async(req,res)=>{
+    const {id, password} = req.body;
+
+    if (!id || !password) {
+        return res.status(400).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+    }
+    let culuser = User.filter(user => user.id == id);
+    if (culuser.length == 0){
+        return res.status(400).send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
+    }
+    if (culuser[0].password != password){
+        return res.status(400).send(util.fail(statusCode.BAD_REQUEST, resMessage.MISS_MATCH_PW));
+    }
+
+    res.status(200).send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, {userId: id}));
+});
+
+router.get('/profile/:id', async(req,res)=>{
+    const id = req.params.id;
+    let culuser = User.filter(user => user.id == id);
+    
+    if (culuser.length == 0){
+        return res.status(400).send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
+    }
+
+    res.status(200).send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, {userId: id, name: culuser[0].name, email: culuser[0].email}));
+
+})
+
+module.exports = router;
